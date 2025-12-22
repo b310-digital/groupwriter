c8bd7238e8b4d97687d92d291ef962ecd32c598b
Jannik Streek <jannik.streek@gmail.com>
2025-12-22 08:42:17 +0100

    fix test config

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,8 +1,11 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     mockReset: true,
+    exclude: [
+      ...configDefaults.exclude,
+      '**/dist/**',
+    ]
   },
 });

2535b0e1095ed056f32a59dbd0e1ee5d5a5a8d7c
JannikStreek <jannik.streek@gmail.com>
2025-12-22 14:01:53 +0100

    set dummy test url

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,8 +1,17 @@
 import { configDefaults, defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
   test: {
     mockReset: true,
     exclude: [...configDefaults.exclude, "**/dist/**"],
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^(\.\.?\/)+generated\/prisma$/,
+        replacement: path.resolve(__dirname, "generated/prisma/client.ts"),
+      },
+    ],
   },
 });

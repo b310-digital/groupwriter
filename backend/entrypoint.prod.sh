9c0d5069a71a6b716569d08b6d9d2709569bc2f8
JannikStreek <jannik.streek@gmail.com>
2026-05-29 19:23:18 +0200

    refactor to pnpm, harden github and docker

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -eu
 
 echo "Creating the schema..."
-npx prisma migrate deploy --config dist/prisma.config.ts
+pnpm exec prisma migrate deploy --config dist/prisma.config.ts
 
 echo "Starting the application..."
-npm run start:prod
+exec pnpm run start:prod
